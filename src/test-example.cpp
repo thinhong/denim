ca2838b644723ae1dd789cc8878d9125f1fcb204
tuyenhn <hntuyen282@gmail.com>
2023-02-28 17:59:52 +0700

    restructure test cases and new test

--- src/test-example.cpp
+++ src/test-example.cpp
@@ -7,38 +7,45 @@
  * `LinkingTo: testthat` within your DESCRIPTION file.
  */
 
-// All test files should include the <testthat.h>
-// header file.
+// All test files should include the <testthat.h> header file.
 #include <testthat.h>
+
 // denim headers
 #include "DistributionConstant.h"
-
-// Normally this would be a function from your package's
-// compiled library -- you might instead just include a header
-// file providing the definition, and let R CMD INSTALL
-// handle building and linking.
-// int twoPlusTwo() {
-//   return 2 + 2;
-// }
+#include "ModelJSON.h"
 
 // Initialize a unit test context. This is similar to how you
 // might begin an R test file with 'context()', expect the
 // associated context should be wrapped in braced.
-context("Sample unit tests") {
-    // The format for specifying tests is similar to that of
-    // testthat's R functions. Use 'test_that()' to define a
-    // unit test, and use 'expect_true()' and 'expect_false()'
-    // to test the desired conditions.
-    // test_that("two plus two equals four") {
-    //   expect_true(twoPlusTwo() == 4);
-    // }
-    test_that("Distribution calcTransitionProbHelper") {
-        DistributionConstant* distribution = new DistributionConstant(0.5);
-        std::vector<double> inputVect{0.3, 0.5, 0.2};
-        size_t size = inputVect.size();
+context("Distribution class") {
+    DistributionConstant distribution(0.5);
+    std::vector<double> inputVect{0.3, 0.5, 0.1, 0.1};
+    size_t idx = 2;
 
-        printf(distribution->calcTransitionProbHelper(inputVect, size))
+    test_that("calcTransitionProbHelper()") {
+        expect_true(distribution.calcTransitionProbHelper(inputVect, idx) == Approx(0.5).margin(0.001));
+        expect_false(distribution.calcTransitionProbHelper(inputVect, idx) == Approx(1).margin(0.001));
+    }
+}
 
-        // expect_true(calcTransitionProbHelper());
+context("ModelJSON") {
+    nlohmann::ordered_json j = nlohmann::ordered_json::parse("{  \"simulationDuration\": 10,  \"errorTolerance\": 0.001,  \"timeStep\": 0.01,  \"initialValues\": {\"S\": 999, \"I\": 1, \"R\": 0},  \"parameters\": {\"beta\": 0.12, \"N\": 1000},  \"transitions\": {  \"S -> I\": {\"distribution\": \"mathExpression\", \"expression\": \"beta * S * I / N\"},  \"I -> R\": {\"distribution\": \"gamma\", \"scale\": 3, \"shape\": 2}}}");
+
+    ModelJSON modeljson(j["initialValues"], j["parameters"], j["transitions"]);
+    auto model = modeljson.getModel();
+    model->sortCompsByInputTransition();
+
+    test_that("getCompsOrder()") {
+        // consume getCompsOrder() standard output
+        std::stringstream buffer;
+        std::streambuf* old = std::cout.rdbuf(buffer.rdbuf());
+
+        expect_true(model->getCompsOrder() == "S I R ");
+        expect_false(model->getCompsOrder() == "I R S ");
+    }
+}
+
+context("Compartment") {
+    test_that("updateCompartment()") {
     }
 }
