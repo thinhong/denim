8ad6dd01d96722830e97b8c0a7d59af1d15c802f
quingzz <quynhanhpt551@gmail.com>
2024-03-12 12:09:25 +0700

    update compare exponential with desolve

--- tests/testthat/test-result.R
+++ tests/testthat/test-result.R
@@ -1,4 +1,4 @@
-test_that("Compare with deSolve", {
+test_that("Compare gamma distribution with desolve", {
     # --- Transition def for denim
     transitions <- list(
     "S -> I" = d_gamma(3, 2)
@@ -25,6 +25,35 @@ test_that("Compare with deSolve", {
     ode_mod <- ode(y = initialValues, times = timestep, parms = parameters, func = transition_func)
 
     tolerance <- 0.0001
+    abs_diff <- abs(mod[, c("Time", "S", "I")] - ode_mod[, c("time", "S", "I")])
+
+    expect_true(all(abs_diff <= tolerance))
+})
+
+test_that("Compare exponential distribution with desolve", {
+    # --- Transition def for denim
+    transitions <- list(
+    "S -> I" = d_exponential(0.2)
+    )
+    parameters <- c(rate=0.2) 
+    initialValues <- c(S = 999, I = 0)
+
+    # --- Transition def for sim
+    transition_func <- function(t, state, param){
+    with(as.list( c(state, param) ), {
+        # rate 
+        dS = -rate*S
+        dI = rate*S
+        list(c(dS,  dI))
+    })
+    }
+
+    # --- compute result using both packages
+    mod <- sim(transitions = transitions, initialValues = initialValues, parameters = parameters, simulationDuration = 10, timeStep = 1)
+    timestep <- seq(0, 10, 1)
+    ode_mod <- ode(y = initialValues, times = timestep, parms = parameters, func = transition_func)
+
+    tolerance <- 0.001
     abs_diff <- abs(mod[, c("Time", "S", "I")] - ode_mod[, c("time", "S", "I")])
 
     expect_true(all(abs_diff <= tolerance))
