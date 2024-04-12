c74ef831817d6e1c6a3813257c2ec9f9db7c23ef
quingzz <quynhanhpt551@gmail.com>
2024-04-12 14:44:14 +0700

    update denim vs desolve

--- tests/testthat/test-result.R
+++ tests/testthat/test-result.R
@@ -1,60 +1,37 @@
-test_that("Compare gamma distribution with desolve", {
+test_that("Compare denim and deSolve", {
     # --- Transition def for denim
     transitions <- list(
-    "S -> I" = d_gamma(3, 2)
+    "S -> I" = d_exponential(0.2),
+    "I -> R" = d_gamma(3, 2)
     )
-    parameters <- c(scale = 3, shape=2) 
-    initialValues <- c(S = 999, S1 = 999, S2 = 0, I = 0)
+    parameters <- c(rate = 0.2, scale = 3, shape=2) 
+    initialValues <- c(S = 999, I = 1, I1 = 1, I2=0, R=0)
 
-    # --- Transition def for sim
-    transition_func <- function(t, state, param){
-        with(as.list( c(state, param) ), {
-            rate = 1/scale
-            # rate of change
-            dS1 = -rate*S1
-            dS2 = rate*S1 - rate*S2
-            dS = dS1 + dS2
-            dI = rate*S2
-            list(c(dS, dS1, dS2,  dI))
-        })
-    }
-
-    # --- compute result using both packages
-    mod <- sim(transitions = transitions, initialValues = initialValues, parameters = parameters, simulationDuration = 10, timeStep = 1)
-    timestep <- seq(0, 10, 1)
-    ode_mod <- ode(y = initialValues, times = timestep, parms = parameters, func = transition_func)
-
-    tolerance <- 0.0001
-    abs_diff <- abs(mod[, c("Time", "S", "I")] - ode_mod[, c("time", "S", "I")])
-
-    expect_true(all(abs_diff <= tolerance))
-})
-
-test_that("Compare exponential distribution with desolve", {
-    # --- Transition def for denim
-    transitions <- list(
-    "S -> I" = d_exponential(0.2)
-    )
-    parameters <- c(rate=0.2) 
-    initialValues <- c(S = 999, I = 0)
-
-    # --- Transition def for sim
+    # --- Transition def for deSolve
     transition_func <- function(t, state, param){
     with(as.list( c(state, param) ), {
-        # rate 
+        gamma_rate = 1/scale
         dS = -rate*S
-        dI = rate*S
-        list(c(dS,  dI))
+        dI1 = rate*S - gamma_rate*I1
+        dI2 = gamma_rate*I1 - gamma_rate*I2
+        dI =  dI1 + dI2
+        dR = gamma_rate*I2
+        list(c(dS, dI, dI1, dI2, dR))
     })
     }
 
-    # --- compute result using both packages
-    mod <- sim(transitions = transitions, initialValues = initialValues, parameters = parameters, simulationDuration = 10, timeStep = 1)
-    timestep <- seq(0, 10, 1)
-    ode_mod <- ode(y = initialValues, times = timestep, parms = parameters, func = transition_func)
+    # --- Timestep definition
+    simulationDuration <- 20 
+    timestep <- 0.0005 # use small timestep to compare with deSolve which has continuous timestep
 
-    tolerance <- 0.001
+    # --- Simulate using both package
+    mod <- sim(transitions = transitions, initialValues = initialValues, parameters = parameters, simulationDuration = simulationDuration, timeStep = timestep)
+    ode_mod <- ode(y = initialValues, times = seq(0, simulationDuration, timestep), parms = parameters, func = transition_func)
+
+
+    ode_mod <- as.data.frame(ode_mod)
+
+    tolerance <- 0.1
     abs_diff <- abs(mod[, c("Time", "S", "I")] - ode_mod[, c("time", "S", "I")])
-
     expect_true(all(abs_diff <= tolerance))
 })
